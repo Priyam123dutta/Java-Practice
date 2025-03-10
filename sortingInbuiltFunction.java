25e02ded4ca0baf930663bd12a72b92a8925ecf9
Priyam Dutta <priyamdutta624@gmail.com>
2025-03-11 01:50:07 +0530

    code complete

--- sortingInbuiltFunction.java
+++ sortingInbuiltFunction.java
@@ -1,4 +1,16 @@
+import java.util.Arrays;
+
 public class sortingInbuiltFunction 
 {
-    
+    public static void main(String[] args)
+    {
+        int[] arr = {1, 4, 78, 56, 28, 61, 91, 37, 59, 611, 748, 627, 1534, 12, 64};
+
+        Arrays.sort(arr);
+
+        for (int i : arr)
+        {
+            System.out.print(i + " ");
+        }
+    }
 }
